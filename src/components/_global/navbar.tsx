896e28b2e499eb42d30512b49a566032cc24c9e0
ilhamhafidz404 <xxspanzie@gmail.com>
2024-05-12 11:57:44 +0700

    Navbar section

---
+++ src/components/_global/navbar.tsx
@@ -0,0 +1,38 @@
+import { IconSun } from "@irsyadadl/paranoid";
+
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between items-center bg-white text-[#111] px-10 py-7 shadow">
+      <div>
+        <h2 className="text-[#111] font-semibold text-xl">WARISAN NUSANTARA</h2>
+      </div>
+      <div className="flex items-center gap-5">
+        <ul className="flex gap-10 border-r-2 border-[#111] pr-6">
+          <li>
+            <a
+              href=""
+              className="text-red-500 relative font-semibold after:content-[''] after:absolute after:-bottom-2 after:left-1/2 after:-translate-x-1/2 after:w-10 after:h-[3px] after:bg-red-500 after:rounded-full"
+            >
+              Beranda
+            </a>
+          </li>
+          <li>
+            <a href="">Destinati</a>
+          </li>
+          <li>
+            <a href="">Jelajah</a>
+          </li>
+          <li>
+            <a href="">Sejarah dan Budaya</a>
+          </li>
+          <li>
+            <a href="">Peta Panduan</a>
+          </li>
+        </ul>
+        <div className="shadow-md flex items-center justify-center rounded-full w-[40px] h-[40px]">
+          <IconSun />
+        </div>
+      </div>
+    </nav>
+  );
+}
